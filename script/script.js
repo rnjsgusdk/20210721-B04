3326819da32264cbad722fa656c5c04eb7e642ec
rnjsgusdk <gusdk0617@naver.com>
2021-07-22 16:25:01 +0900

    20210722-B04

--- script/script.js
+++ script/script.js
@@ -1,11 +1,31 @@
 $(function () {
+// GNB시작
+    $(".mainLi a,#subMeunBG").mouseover(function(){
+        $(".subMenu").stop().slideDown(800);
+        $("div#submeunbg").stop().slideDown(800);
+    });
+    $(".mainLi a,#subMeunBG").mouseover(function(){
+        $(".subMenu").stop().slideUp(300);
+        $("div#subMeunBG").stop().slideUp(300);
+    });
+// GNB끝
 
-    $(".mainli a,#submeunbg").mouseover(function(){
-        $(".mainli a").stop().slidedown(800);
-        $("div#submeunbg").slidedown(800);
-    });
-    $(".mainli a,#submeunbg").mouseover(function(){
-        $(".mainli a").stop().slideup(800);
-        $("div#submeunbg").slideup(800);
-    });
+
+// Slide시작
+ setInterval(fnSlide, 3000);
+
+ function fnSlide(){
+ $ ("#shuttleframe").animate(
+     {"margin-left":"-1200px"},
+     2400,
+     function(){
+         $("#silde a:frist-child").insertAfter("silde a:last-child");
+         $("#shuttleframe").css({"margin-left": "0"});
+
+     }
+ );
+
+}
+// Slide끝
+
 });
